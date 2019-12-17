5471db78e950328a29913ca5bb67cf1cf7ddd931
Ken Ogura <nekarugo628@gmail.com>
2019-12-17 20:53:22 +0900

    beat the jeans and get Pi

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,40 @@ fn main() {
         }),
         mask: None,
     };
+
+    let update_request = proto::TestMsg {
+        num: 314,
+        str: String::default(),
+        hito: Some(proto::Hito {
+            age: u32::default(),
+            name: String::default(),
+            fuku: Some(proto::Fuku {
+                size: u32::default(),
+                name: "damage jeans".to_string(),
+            }),
+        }),
+        mask: Some(google::protobuf::FieldMask {
+            paths: vec!["/num".to_string(), "/hito/fuku/name".to_string()],
+        }),
+    };
+
     let mut json_val = serde_json::to_value(&test_msg).unwrap();
+    let json_req = serde_json::to_value(&update_request).unwrap();
+
     println!("{}", json_val);
-    let fuku_name = json_val.pointer_mut("/hito/fuku/name").unwrap();
-    *fuku_name = serde_json::value::Value::String("damage jeans".to_string());
+
+    batch(&mut json_val, &json_req);
+
     println!("{}", json_val);
+}
+
+fn batch(val: &mut serde_json::Value, req: &serde_json::Value) {
+    if let serde_json::Value::Array(paths) = req.pointer("/mask/paths").unwrap() {
+        for path in paths {
+            if let serde_json::Value::String(path_str) = path {
+                let field = val.pointer_mut(&path_str).unwrap();
+                *field = req.pointer(&path_str).unwrap().clone();
+            }
+        }
+    }
 }
